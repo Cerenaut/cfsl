dc31cd6eea03dd32fde860cfd6a36bb22a4341b7
Abdelrahman Ahmed <181392+abdel@users.noreply.github.com>
2020-11-17 09:48:17 +1100

    add future to pip install

--- frameworks/cfsl/install.sh
+++ frameworks/cfsl/install.sh
@@ -2,4 +2,4 @@ conda install -qy -c conda-forge opencv
 conda install -qy numpy scipy matplotlib
 conda install -qy -c conda-forge pbzip2
 conda install -qy pillow tqdm
-pip install -q tensorboard
+pip install -q tensorboard future
